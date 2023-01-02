aa9f0cddf9e6e6894396815ce8d95769be015778
unnat bharol <unnatbharol@gmail.com>
2023-01-02 14:19:37 +0530

    fixed toggler

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,41 @@
 
 // alert(window.innerHeight);
 document.getElementById("actualtoggle").click();
-// alert(window.innerWidth);
 var keep = 0
-document.getElementById("actualtoggle").addEventListener("click",check);
+document.getElementById("actualtoggle").addEventListener("click", check);
 function check() {
-    var idtest = document.getElementById("dismissible");
-    if (idtest) {
-        document.getElementById("dismissible").innerHTML = "" ; 
-        document.getElementById("switcher").className="toggleoffnow" ;  
-        document.getElementById("switcher").setTimeout(() => {
-            document.getElementById("switcher").className="toggledisplay" ; 
-        }, 1000); 
-        keep = 1;
-        return ; 
-    }
-    var classtest = document.getElementsByClassName("style-scope ytd-rich-shelf-renderer");
-    if(classtest.length>0)
+    if (keep == 0) 
     {
-        document.getElementsByClassName("style-scope ytd-rich-shelf-renderer")[0].innerHTML = "";
-        document.getElementById("switcher").className="toggleoffnow" ;  
-        document.getElementById("switcher").setTimeout(() => {
-            document.getElementById("switcher").className="toggledisplay" ; 
-        }, 1000); 
-        keep = 1;
-        return ; 
+        var idtest = document.getElementById("dismissible");
+        if (idtest) 
+        {
+            document.getElementById("dismissible").innerHTML = "";
+            var x = document.getElementById("switcher").className;
+            document.getElementById("switcher").className = document.getElementById("switcheron").className;
+            document.getElementById("switcheron").className = x;
+            return;
+        }
+        var classtest = document.getElementsByClassName("style-scope ytd-rich-shelf-renderer");
+        if (classtest.length > 0) 
+        {
+            document.getElementsByClassName("style-scope ytd-rich-shelf-renderer")[0].innerHTML = "";
+            var x = document.getElementById("switcher").className;
+            document.getElementById("switcher").className = document.getElementById("switcheron").className;
+            document.getElementById("switcheron").className = x;
+            return;
+        }
+        var custom = document.getElementsByTagName("ytd-rich-shelf-renderer")
+        if (custom.length > 0) 
+        {
+            document.getElementsByTagName("ytd-rich-shelf-renderer")[0].innerHTML = "";
+            var x = document.getElementById("switcher").className;
+            document.getElementById("switcher").className = document.getElementById("switcheron").className;
+            document.getElementById("switcheron").className = x;
+            return;
+        }
     }
-    var custom = document.getElementsByTagName("ytd-rich-shelf-renderer")
-    if (custom.length>0) {
-        document.getElementsByTagName("ytd-rich-shelf-renderer")[0].innerHTML="" ; 
-        document.getElementById("switcher").className="toggleoffnow" ;  
-        document.getElementById("switcher").setTimeout(() => {
-            document.getElementById("switcher").className="toggledisplay" ; 
-        }, 1000); 
-        keep = 1;
-        return ; 
+    else
+    {
+        
     }
-    console.log("wohsitehaihini");
-}
-
+}   
