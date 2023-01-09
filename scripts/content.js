00c6bd5e626312223265dc474710b9edea112ff5
unnat bharol <unnatbharol@gmail.com>
2023-01-09 23:56:55 +0530

    things work okok

--- scripts/content.js
+++ scripts/content.js
@@ -27,47 +27,50 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
                 var k = document.querySelectorAll("a[href*='/shorts']")
                 for (let card = 0; card < k.length; card++) {
                     try {
-                        k[card].parentElement.parentElement.removeAttribute('hidden') ;
+                        k[card].parentElement.parentElement.removeAttribute('hidden');
                     }
                     catch {
-                        k[card].parentElement.removeAttribute('hidden') ;
+                        k[card].parentElement.removeAttribute('hidden');
                     }
                 }
 
             }
             sendResponse("made changes");
         }
+        else if (message = "pager") {
+            var y = browser.storage.local.get("state");
+            var x = await y;
+            x = x.state;
+            if (x == 1) {
+                if (window.location.href.includes("youtube.com/shorts") == true) {
+                    window.history.back();
+                }
+                var k = document.querySelectorAll("a[href*='/shorts']")
+                for (let card = 0; card < k.length; card++) {
+                    try {
+                        k[card].parentElement.parentElement.hidden = "true";
+                    }
+                    catch {
+                        k[card].parentElement.hidden = "true";
+                    }
+        
+                }
+            }
+            else if (x == 0) {
+                var k = document.querySelectorAll("a[href*='/shorts']")
+                for (let card = 0; card < k.length; card++) {
+                    try {
+                        k[card].parentElement.parentElement.removeAttribute('hidden');
+                    }
+                    catch {
+                        k[card].parentElement.removeAttribute('hidden');
+                    }
+                }
+        
+            }
+            sendResponse("made changes");
+        }
 
     })();
     return true;
-}) 
-window.addEventListener("DOMContentLoaded",pol)
-var pol = (async ()  =>{
-        var y = browser.storage.local.get("state");
-        var x = await y;
-        x = x.state;
-        if (x == 1) {
-            var k = document.querySelectorAll("a[href*='/shorts']")
-            for (let card = 0; card < k.length; card++) {
-                try {
-                    k[card].parentElement.parentElement.hidden = "true";
-                }
-                catch {
-                    k[card].parentElement.hidden = "true";
-                }
-
-            }
-        }
-        else if (x == 0) {
-            var k = document.querySelectorAll("a[href*='/shorts']")
-            for (let card = 0; card < k.length; card++) {
-                try {
-                    k[card].parentElement.parentElement.removeAttribute('hidden') ;
-                }
-                catch {
-                    k[card].parentElement.removeAttribute('hidden') ;
-                }
-            }
-
-        }
 })
