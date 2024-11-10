371298921fd424c3dc0aeb11055bffcdad72fbef
dxuian <deathxunnat@gmail.com>
2024-11-10 07:13:54 +0530

    ads

--- scripts/content.js
+++ scripts/content.js
@@ -5,72 +5,72 @@ var browser;
 if (typeof browser === "undefined") {
     var browser = chrome;
 }
-browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    (async () => {
-        if (message == "changetype") {
-            var y = browser.storage.local.get("state");
-            var x = await y;
-            x = x.state;
-            if (x == 0) {
-                var k = document.querySelectorAll("a[href*='/shorts']")
-                for (let card = 0; card < k.length; card++) {
-                    try {
-                        k[card].parentElement.parentElement.hidden = "true";
-                    }
-                    catch {
-                        k[card].parentElement.hidden = "true";
-                    }
 
+async function handleasyncmessage(message, sender, sendResponse)
+{
+    if (message == "changetype") {
+        var y = browser.storage.local.get("state");
+        var x = await y;
+        x = x.state;
+        if (x == 0) {
+            var k = document.querySelectorAll("a[href*='/shorts']")
+            for (let card = 0; card < k.length; card++) {
+                try {
+                    k[card].parentElement.parentElement.hidden = "true";
+                }
+                catch {
+                    k[card].parentElement.hidden = "true";
                 }
             }
-            else if (x == 1) {
-                var k = document.querySelectorAll("a[href*='/shorts']")
-                for (let card = 0; card < k.length; card++) {
-                    try {
-                        k[card].parentElement.parentElement.removeAttribute('hidden');
-                    }
-                    catch {
-                        k[card].parentElement.removeAttribute('hidden');
-                    }
-                }
-
-            }
-            sendResponse("made changes");
         }
-        else if (message = "pager") {
-            var y = browser.storage.local.get("state");
-            var x = await y;
-            x = x.state;
-            if (x == 1) {
-                if (window.location.href.includes("youtube.com/shorts") == true) {
-                    window.history.back();
+        else if (x == 1) {
+            var k = document.querySelectorAll("a[href*='/shorts']")
+            for (let card = 0; card < k.length; card++) {
+                try {
+                    k[card].parentElement.parentElement.removeAttribute('hidden');
                 }
-                var k = document.querySelectorAll("a[href*='/shorts']")
-                for (let card = 0; card < k.length; card++) {
-                    try {
-                        k[card].parentElement.parentElement.hidden = "true";
-                    }
-                    catch {
-                        k[card].parentElement.hidden = "true";
-                    }
-        
+                catch {
+                    k[card].parentElement.removeAttribute('hidden');
                 }
             }
-            else if (x == 0) {
-                var k = document.querySelectorAll("a[href*='/shorts']")
-                for (let card = 0; card < k.length; card++) {
-                    try {
-                        k[card].parentElement.parentElement.removeAttribute('hidden');
-                    }
-                    catch {
-                        k[card].parentElement.removeAttribute('hidden');
-                    }
-                }
-        
-            }
-            sendResponse("made changes");
         }
-
-    })();
+        sendResponse("made changes");
+    }
+    else if (message == "pager") {
+        var y = browser.storage.local.get("state");
+        var x = await y;
+        x = x.state;
+        if (x == 1) {
+            if (window.location.href.includes("youtube.com/shorts") == true) {
+                window.history.back();
+            }
+            var k = document.querySelectorAll("a[href*='/shorts']")
+            for (let card = 0; card < k.length; card++) {
+                try {
+                    k[card].parentElement.parentElement.hidden = "true";
+                }
+                catch {
+                    k[card].parentElement.hidden = "true";
+                }
+            }
+        }
+        else if (x == 0) {
+            var k = document.querySelectorAll("a[href*='/shorts']")
+            for (let card = 0; card < k.length; card++) {
+                try {
+                    k[card].parentElement.parentElement.removeAttribute('hidden');
+                }
+                catch {
+                    k[card].parentElement.removeAttribute('hidden');
+                }
+            }
+        }
+        sendResponse("made changes");
+    }
     return true;
-})
+}
+async function handleMessage(message, sender, sendResponse) {
+    return await handleasyncmessage(message, sender, sendResponse);
+}
+
+browser.runtime.onMessage.addListener(handleMessage);
